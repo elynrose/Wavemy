01f1d57708399c8badc3d3b29d805353ee785c11
elyayertey@gmail.com <elyayertey@gmail.com>
2025-09-09 10:39:47 -0400

    removed sensitive information

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,10 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
+import webpack from 'webpack';
+import dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,4 +21,14 @@ export default {
   resolve: {
     extensions: ['.js'],
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+      'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+      'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+      'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+      'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+      'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
+    }),
+  ],
 };
